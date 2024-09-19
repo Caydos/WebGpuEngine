6cdb8576640ceb83790c4e959fb026c2fffd11a7
Caydos <helde376@gmail.com>
2024-09-19 03:34:17 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+async function initWebGPU() {
+     if (!navigator.gpu) {
+          console.error("WebGPU is not supported on this browser.");
+          return;
+     }
+
+     const adapter = await navigator.gpu.requestAdapter();
+     if (!adapter) {
+          console.error("Failed to get GPU adapter.");
+          return;
+     }
+
+     const device = await adapter.requestDevice();
+     console.log("Nuhh huh it seems to work", device);
+}
+
+initWebGPU();
